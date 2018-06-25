93bd8abfd7286375fe1f842938160feccdd9d476
kevin <kevinelsen@hotmail.fr>
2018-06-25 16:22:14 +0200

    exo2

---
+++ script.js
@@ -0,0 +1,10 @@
+//Déclaration de la fonction.
+function ontext() {
+//Déclaration d'une variable.
+document.getElementById('text').style.display = 'inline';
+}
+//Déclaration de la fonction.
+function offtext() {
+//Déclaration d'une variable.
+document.getElementById('text').style.display = 'none';
+}
